5c03964bf5a7523098cb366ade41b37448690c0d
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-18 15:34:35 +0200

    feat(): import "playeraction" component

--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayeractionComponent } from './playeraction/playeraction.component';
+import { PlayerstatsComponent } from './playerstats/playerstats.component';
 
 
 
 @NgModule({
-  declarations: [PlayeractionComponent],
+  declarations: [PlayeractionComponent,PlayerstatsComponent],
   imports: [
     CommonModule
   ],
