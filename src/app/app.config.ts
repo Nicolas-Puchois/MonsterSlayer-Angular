bba935a6c85f029e5da451e777f2b0fa85985b51
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-18 15:43:46 +0200

    feat(app.config.ts): import prebuild component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { PlayerModule } from './player/player.module';
+import { importProvidersFrom } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
