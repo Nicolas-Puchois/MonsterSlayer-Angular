98d97ea3aff1111ff610cf5ce0573a4b60ad5271
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-18 15:22:50 +0200

    feat(): create component "playerstats"

---
+++ src/app/player/playerstats/playerstats.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playerstats',
+  standalone: true,
+  imports: [],
+  templateUrl: './playerstats.component.html',
+  styleUrl: './playerstats.component.css'
+})
+export class PlayerstatsComponent {
+
+}
