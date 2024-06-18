f03b38e85ff60b8a3aa8ee8df63fff408878de05
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-18 15:25:24 +0200

    feat(): remove standalone of "playerstats" component

--- src/app/player/playerstats/playerstats.component.ts
+++ src/app/player/playerstats/playerstats.component.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-playerstats',
-  standalone: true,
-  imports: [],
   templateUrl: './playerstats.component.html',
   styleUrl: './playerstats.component.css'
 })
 export class PlayerstatsComponent {
-
+  life : number = 100;
 }
