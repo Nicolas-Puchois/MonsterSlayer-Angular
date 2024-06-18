d498d5c4d855c54b7a14e24d6b53c427c823d3f0
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-18 15:25:17 +0200

    feat(): remove standalone of "playeraction" component

--- src/app/player/playeraction/playeraction.component.ts
+++ src/app/player/playeraction/playeraction.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-playeraction',
-  standalone: true,
-  imports: [],
   templateUrl: './playeraction.component.html',
   styleUrl: './playeraction.component.css'
 })
