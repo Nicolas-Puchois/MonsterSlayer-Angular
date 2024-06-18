d18b08bd1545538be2053188ce2027b402287b7b
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-18 15:22:57 +0200

    feat(): create component "playeraction"

---
+++ src/app/player/playeraction/playeraction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playeraction',
+  standalone: true,
+  imports: [],
+  templateUrl: './playeraction.component.html',
+  styleUrl: './playeraction.component.css'
+})
+export class PlayeractionComponent {
+
+}
