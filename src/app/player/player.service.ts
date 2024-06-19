9b9a72d0b7144f091022df618ecd7b5720aaf931
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-19 14:19:05 +0200

    feat(): add "healAction" method

--- src/app/player/player.service.ts
+++ src/app/player/player.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +8,25 @@ export class PlayerService {
   public randomNumber: number = 1;
   public min : number = 1;
   public max : number = 20;
+  public lifeValue : number = 50;
+  public healValue : number = 1;
+
     constructor() { }
+
+  getLifeValue(){
+    return this.lifeValue
+  }
 
   randomGenerator(){
      this.randomNumber = Math.floor(Math.random()*(this.max - this.min + 1));
      return this.randomNumber
+  }
+
+  healAction(){
+    this.randomGenerator();
+    this.healValue = this.randomNumber;
+    this.lifeValue = Math.min(this.lifeValue + this.healValue, 100);    
+    return this.lifeValue
   }
 
 }
