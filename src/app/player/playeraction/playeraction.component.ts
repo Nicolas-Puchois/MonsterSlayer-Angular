b4bf10dfd2b293be9b91b6ff49959dec9b1f0c85
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-20 09:36:36 +0200

    feat(): call "attackAction" in component

--- src/app/player/playeraction/playeraction.component.ts
+++ src/app/player/playeraction/playeraction.component.ts
@@ -1,42 +1,38 @@
 import { Component } from '@angular/core';
 import { PlayerService } from '../player.service';
 
-
 @Component({
   selector: 'app-playeraction',
   templateUrl: './playeraction.component.html',
-  styleUrl: './playeraction.component.css'
+  styleUrl: './playeraction.component.css',
 })
 export class PlayeractionComponent {
   attack: number;
-  superAttack: number;
-  lifeValue : number;
+  lifeValue: number;
+  superAttack: number = 0;
 
-  constructor(public PlayerService : PlayerService){
-    this.attack = this.PlayerService.randomNumber;
-    this.superAttack = this.PlayerService.randomNumber;
+  constructor(public PlayerService: PlayerService) {
     this.lifeValue = this.PlayerService.lifeValue;
+    this.attack = this.PlayerService.attackValue;
   }
 
-
-
-  attackAction(){
-    this.PlayerService.randomGenerator();
-    this.attack = this.PlayerService.randomNumber;
+  attackAction() {
+    this.PlayerService.attackAction();
+    this.attack = this.PlayerService.attackValue;
   }
 
-  superAttackAction(){
-    this.PlayerService.randomGenerator();
-    this.superAttack = this.PlayerService.randomNumber;
-    this.superAttack += 15
+  superAttackAction() {
+    this.PlayerService.attackAction();
+    this.superAttack = this.PlayerService.attackValue
+    this.superAttack += 15;
   }
 
-  healAction(){
-      this.PlayerService.healAction();
-    }
+  healAction() {
+    this.PlayerService.healAction();
+  }
 
-    giveUpAction(){
-      this.PlayerService.giveupAction();
-      alert("you surrender")
-    }
+  giveUpAction() {
+    this.PlayerService.giveupAction();
+    alert('you surrender');
+  }
 }
