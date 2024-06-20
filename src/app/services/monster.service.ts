1dac717cdc6bdcd289595139e244177a222d0958
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-20 11:32:48 +0200

    feat(): add "randomGenerator" method

--- src/app/services/monster.service.ts
+++ src/app/services/monster.service.ts
@@ -6,9 +6,15 @@ import { Injectable } from '@angular/core';
 export class MonsterService {
   public lifeValue : number = 100;
   public damagetaken: number = 1;
+  public randomNumber: number = 1;
+  public min : number = 1;
+  public max : number = 20;
 
-
-
+  randomGenerator(){
+    this.randomNumber = Math.floor(Math.random()*(this.max - this.min + 1));
+    return this.randomNumber
+ }
+ 
   getLifeValue(){
     return this.lifeValue
   }
