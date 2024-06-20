154ef0eafb51d3a73de0f55b1abbf3d63f61cbf6
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-20 15:32:25 +0200

    feat(): add "chechWinner" and "reset" method

--- src/app/services/reset.service.ts
+++ src/app/services/reset.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@angular/core';
+import { MonsterService } from './monster.service';
+import { PlayerService } from './player.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResetService {
-  lifeplayer: number = 100;
-  lifemonster: number = 100;
 
-  constructor() { }
+  resetlifeplayer: number = 100;
+  resetlifemonster: number = 100;
 
-  giveupAction() {
-    this.lifeplayer = this.lifeplayer;
-    this.lifemonster = this.lifemonster;
+  constructor(private MonsterService: MonsterService, private PlayerService: PlayerService) { }
+
+  checkWinner(){
+    if(this.MonsterService.lifevalue <= 0){
+      alert("Player win");
+      this.resetAction();
+    }else if( this.PlayerService.lifevalue <= 0){
+      alert("Monster Win");
+      this.resetAction();
+    }
   }
+
+  resetAction() {
+    this.MonsterService.lifevalue = this.resetlifeplayer;
+    this.PlayerService.lifevalue = this.resetlifemonster;
+  }
+
+
 }
