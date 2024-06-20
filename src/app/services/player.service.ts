b1c4eaa3ccf375077fcfcd9ecf9233391dfa0c63
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-20 12:00:28 +0200

    feat(): add "randomGenerator" method for "player" services

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -4,16 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class PlayerService {
-
   lifevalue: number = 50;
   healvalue: number = 1;
 
+  randomnumber: number = 1;
+  min: number = 1;
+  max: number = 20;
+
+  randomGenerator() {
+    this.randomnumber = Math.floor(Math.random() * (this.max - this.min + 1));
+    return this.randomnumber;
+  }
 
   getLifeValue() {
     return this.lifevalue;
   }
-
-
 
   healAction() {
     this.randomGenerator();
