3bbbda88cff42354af6068425623682c1f7967a7
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-06-21 09:21:35 +0200

    feat(): import "combat" service

--- src/app/services/monster.service.ts
+++ src/app/services/monster.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { CombatsService } from './combats.service';
 
 @Injectable({
   providedIn: 'root',
@@ -7,12 +8,20 @@ export class MonsterService {
   lifevalue: number = 100;
   damagetaken: number = 1;
 
+  constructor(private CombatService: CombatsService){}
 
   getLifeValue() {
     return this.lifevalue;
   }
 
   damagetoLife() {
+    this.damagetaken = this.CombatService.attackvalue
+    this.lifevalue -= this.damagetaken;
+    return this.lifevalue;
+  }
+
+  superDamagetolife(){
+    this.damagetaken = this.CombatService.superattackvalue;
     this.lifevalue -= this.damagetaken;
     return this.lifevalue;
   }
